6a4d81a0c6999ab48e7d908ccc9a14c8f07ccaa2
Volodymyr Kyryliuk <91565516+Neoxenus@users.noreply.github.com>
2022-05-26 13:05:37 +0300

    added decorator

--- src/main/java/my/decorator/Decorators.java
+++ src/main/java/my/decorator/Decorators.java
@@ -1,11 +1,15 @@
 package my.decorator;
 
+import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Decorators {
     public static List<String> evenIndexElementsSubList(List<String> sourceList) {
-        throw new UnsupportedOperationException();
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < sourceList.size(); i+=2) {
+            res.add(sourceList.get(i));
+        }
+        return res;
     }
-
-
 }
